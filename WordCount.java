1ce3dff695f3e30241b9181c751b26ced31014d8
Wang312412 <2834672662@qq.com>
2024-05-12 10:46:29 +0800

    首次提交

--- WordCount.java
+++ WordCount.java
@@ -9,22 +9,16 @@ public class WordCount {
 
         try
         {
-            //创建文件对象和读取对象
+
             File in=new File(fileName);
             FileReader reader=new FileReader(in);
 
-            //读取文本文件到字符数组letters
             char[] letters=new char[(int)in.length()];
             reader.read(letters);
 
-            //统计单词数量
+
             String s=String.valueOf(letters);//char[]转String
-            /*
-             * 下面几个函数的含义：
-             * replaceAll(String regex, String replacement):用replacement替换所有的regex匹配项
-             * trim():去掉一段String首尾空格
-             * split():根据给定正则来分割字符串
-             */
+
             String[ ] words=s.replaceAll("[^a-zA-Z]+"," ").trim( ).split(" ");
 
             System.out.println("字符数量："+letters.length+"\n单词数量："+words.length);
